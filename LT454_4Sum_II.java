28e884fa4d9296c54ad3a7034f52099aaff1217f
oblivionxx <oblivionxx@users.noreply.github.com>
2017-08-13 20:47:58 +0100

    Update LT454_4Sum_II.java

--- LT454_4Sum_II.java
+++ LT454_4Sum_II.java
@@ -25,26 +25,22 @@ Binary Search, Hash Table
  */
 public class LT454_4Sum_II {
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        Map<Integer,Integer> sums = new HashMap<>();
-    	int count = 0;
-    	for(int i=0; i<A.length;i++) {
-    		for(int j=0;j<B.length;j++){
-    			int sum = A[i]+B[j];
-    			if(sums.containsKey(sum)) {
-    				sums.put(sum, sums.get(sum)+1);
-    			} else {
-    				sums.put(sum, 1);
-    			}
-    		}
-    	}
-    	for(int k=0; k<A.length;k++) {
-    		for(int z=0;z<C.length;z++){
-    			int sum = -(C[k]+D[z]);
-    			if(sums.containsKey(sum)) {
-    				count+=sums.get(sum);
-    			}
-    		}
-    	}
-    	return count;
+        Map<Integer, Integer> map = new HashMap<>();
+    
+        for(int i=0; i<C.length; i++) {
+            for(int j=0; j<D.length; j++) {
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int res=0;
+        for(int i=0; i<A.length; i++) {
+            for(int j=0; j<B.length; j++) {
+                res += map.getOrDefault(-1 * (A[i]+B[j]), 0);
+            }
+        }
+
+        return res;
     }
 }
