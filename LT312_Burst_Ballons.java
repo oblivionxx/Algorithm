957f13804d915698d806c7195babf51f8e26478e
oblivionxx <oblivion.meteor@gmail.com>
2017-08-26 16:14:42 +0100

    Update dp solution

--- LT312_Burst_Ballons.java
+++ LT312_Burst_Ballons.java
@@ -19,59 +19,58 @@ Return 167
 Divide and Conquer, DP
  */
 public class LT312_Burst_Ballons {
-	//O(n^3)
-	public int maxCoins(int[] nums) {
-        //https://leetcode.com/discuss/72216/share-some-analysis-and-explanations
-        int len = nums.length;
-        int[] iNum = new int[len+2];
-        for(int i=1;i<=len;i++){
-            iNum[i] = nums[i-1];
-        }
-        iNum[0] =1;
-        iNum[len+1] = 1;
+    // O(n^3)
+    public int maxCoins(int[] iNums) {
+	// O(n^3) coins you get for a balloon does not depend on the balloons already burst. Therefore instead of divide the problem by the first balloon to burst, we divide the problem by the last
+	// balloon to burst
+	// Find the maximal coins of a subrange by trying every possible final burst within that range
+	int[] nums = new int[iNums.length + 2];
+	int n = 1;
+	for (int x : iNums)
+	    if (x > 0)
+		nums[n++] = x;
+	nums[0] = nums[n++] = 1; // transform nums array to be {1,num[0],...num[n-1],1}
 
-    
-        int[][] dp = new int[len+2][len+2];
-        for(int k=2;k<len+2;k++){
-            for(int left=0;left<len+2-k;left++){
-                int right = left+k;
-                for(int i=left+1;i<right;i++){
-                    dp[left][right] = Math.max(dp[left][right], iNum[i]*iNum[left]*iNum[right]+dp[left][i]+dp[i][right]);
-                }
-            }
-        }
-        
+	int[][] dp = new int[n][n];
+	for (int k = 2; k < n; ++k)
+	    for (int left = 0; left < n - k; ++left) {
+		int right = left + k;
+		for (int i = left + 1; i < right; ++i)
+		    dp[left][right] = Math.max(dp[left][right],
+			    nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]);
+	    }
 
-        return dp[0][len+1];
+	return dp[0][n - 1];
     }
-	
-	//<O(2^n) divide and conquer + memorization
-	int[][] dp;
-	int[] values;
+
+    // <O(2^n) divide and conquer + memorization
+    int[][] dp;
+    int[] values;
+
     public int maxCoins2(int[] nums) {
-        if (nums == null || nums.length == 0) {
-        	return 0;
-        }
-        int n = nums.length;
-        dp = new int[n + 2][n + 2];
+	if (nums == null || nums.length == 0) {
+	    return 0;
+	}
+	int n = nums.length;
+	dp = new int[n + 2][n + 2];
 
-        //Initialize new array
-        values = new int[n + 2];
-        values[0] = values[n + 1] = 1;
-        for (int i = 1; i < n + 1; i++) {
-        	values[i] = nums[i - 1];
-        }
-       
-        return DP(1, n);
+	// Initialize new array
+	values = new int[n + 2];
+	values[0] = values[n + 1] = 1;
+	for (int i = 1; i < n + 1; i++) {
+	    values[i] = nums[i - 1];
+	}
+
+	return DP(1, n);
     }
 
-    public int DP(int i, int j){
-    	if (dp[i][j] > 0) {//memorization
-    		return dp[i][j];
-    	}
-    	for (int x = i; x <= j; x++) {              //cut at x between [i,j]
-    		dp[i][j] = Math.max(dp[i][j], DP(i, x - 1) + values[i-1]*values[x]*values[j+1] + DP(x + 1, j));
-    	}
-    	return dp[i][j];
+    public int DP(int i, int j) {
+	if (dp[i][j] > 0) {// memorization
+	    return dp[i][j];
+	}
+	for (int x = i; x <= j; x++) { // cut at x between [i,j]
+	    dp[i][j] = Math.max(dp[i][j], DP(i, x - 1) + values[i - 1] * values[x] * values[j + 1] + DP(x + 1, j));
+	}
+	return dp[i][j];
     }
 }
